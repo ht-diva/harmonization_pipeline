adefa52d616c49a9168e4eeb6818e2bd66354c22
Gianmauro Cuccuru <gianmauro.cuccuru@fht.org>
2024-02-11 08:42:51 +0100

    set resources

--- workflow/scripts/bgzip_tabix.sh
+++ workflow/scripts/bgzip_tabix.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 INPUT=$1
+THREADS=$2
 DIRNAME=`dirname ${1}`
 FILENAME=`basename ${1} .gz`
 
 
-gunzip ${INPUT} && bgzip ${DIRNAME}/${FILENAME} && tabix -f -S 1 -s 1 -b 2 -e 2 ${INPUT}
+gunzip ${INPUT} && bgzip --threads ${THREADS} ${DIRNAME}/${FILENAME} && tabix -f -S 1 -s 1 -b 2 -e 2 ${INPUT}
