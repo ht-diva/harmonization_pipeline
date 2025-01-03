694da621e2bb0c15ab44bb1c00682cbe50240b2c
Gianmauro Cuccuru <gianmauro.cuccuru@fht.org>
2025-01-03 11:33:04 +0100

    update filtering_by_snipid script

--- workflow/scripts/filtering_by_snipid.py
+++ workflow/scripts/filtering_by_snipid.py
@@ -4,14 +4,19 @@ from pathlib import Path
 
 @click.command()
 @click.option("-i", "--input_path", required=True, help="Input path")
+@click.option("--input_separator", default='\t', help="Input separator")
+@click.option("--input_compression", default=False, is_flag=True, help="Input compression")
 @click.option("-o", "--output_path", required=True, help="Output path")
 @click.option("-f", "--filter_path", required=True, help="Snipid to filter path")
-def main(input_path, output_path, filter_path):
+def main(input_path, input_separator, input_compression, output_path, filter_path):
     input = Path(input_path)
     snpid = Path(filter_path)
 
     snpid_df = pd.read_csv(snpid, sep='\t')
-    sumstat_df = pd.read_csv(input, sep='\t', compression='gzip')
+    if input_compression:
+        sumstat_df = pd.read_csv(input, sep=input_separator, compression='gzip')
+    else:
+        sumstat_df = pd.read_csv(input, sep=input_separator)
 
     sumstat_df = sumstat_df[~sumstat_df['SNPID'].isin(snpid_df['SNPID'])]
 
