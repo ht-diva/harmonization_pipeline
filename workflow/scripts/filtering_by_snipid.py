bb2b4cb376105723c0eb65a42b02b70614055ad3
Gianmauro Cuccuru <gianmauro.cuccuru@fht.org>
2025-01-03 15:24:28 +0100

    update filtering_by_snipid script

--- workflow/scripts/filtering_by_snipid.py
+++ workflow/scripts/filtering_by_snipid.py
@@ -6,9 +6,11 @@ from pathlib import Path
 @click.option("-i", "--input_path", required=True, help="Input path")
 @click.option("--input_separator", default='\t', help="Input separator")
 @click.option("--input_compression", default=False, is_flag=True, help="Input compression")
+@click.option("--input_snpid_column", default='SNP', help="Input SNPID column")
 @click.option("-o", "--output_path", required=True, help="Output path")
 @click.option("-f", "--filter_path", required=True, help="Snipid to filter path")
-def main(input_path, input_separator, input_compression, output_path, filter_path):
+@click.option("--filter_snpid_column", default='ID', help="filter SNPID column")
+def main(input_path, input_separator, input_compression, input_snpid_column, output_path, filter_path, filter_snpid_column):
     input = Path(input_path)
     snpid = Path(filter_path)
 
@@ -18,7 +20,7 @@ def main(input_path, input_separator, input_compression, output_path, filter_pat
     else:
         sumstat_df = pd.read_csv(input, sep=input_separator)
 
-    sumstat_df = sumstat_df[~sumstat_df['SNPID'].isin(snpid_df['SNPID'])]
+    sumstat_df = sumstat_df[~sumstat_df[input_snpid_column].isin(snpid_df[filter_snpid_column])]
 
     sumstat_df.to_csv(output_path, sep='\t', compression='gzip', index=False)
 
