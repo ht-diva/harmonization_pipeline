be99d7435686661d0d9883c994ee9162750d22a8
deborahzani <deborah.zani@fht.org>
2025-06-19 14:39:13 +0200

    update ukb list and force gzip for tabix

--- workflow/scripts/bgzip_tabix.sh
+++ workflow/scripts/bgzip_tabix.sh
@@ -6,4 +6,4 @@ DIRNAME=`dirname ${1}`
 FILENAME=`basename ${1} .gz`
 
 # tabix indexes CHROM and POS columns
-gunzip ${INPUT} && bgzip --threads ${THREADS} ${DIRNAME}/${FILENAME} && tabix -f -S 1 -s 1 -b 2 -e 2 ${INPUT}
+gunzip -f ${INPUT} && bgzip -f --threads ${THREADS} ${DIRNAME}/${FILENAME} && tabix -f -S 1 -s 1 -b 2 -e 2 ${INPUT}
